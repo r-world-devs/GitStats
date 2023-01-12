219a85e7980d0dadc788f289884faae5174ff395
maciekbanas <74212933+maciekbanas@users.noreply.github.com>
2023-01-12 15:35:10 +0100

    Deprecate way of setting connections by wrapping GitHub and GitLab ob… (#26)
    * Deprecate way of setting connections by wrapping GitHub and GitLab objects in GitStats along with adjusting tests.
    
    After introducing new way of setting connections with set_connection method, an old way with creating new GitHub and GitLab objects and passing them to GitStats initialize method is no longer needed.
    
    * Fix CMD-R-check.

--- tests/testthat/test-class_gitlab.R
+++
@@ -1,9 +0,0 @@
-git_lab_wrong_groups <- GitLabClient$new(
-  groups = "Avengers",
-  rest_api_url = "https://gitlab.com/api/v4",
-  token = Sys.getenv("GITLAB_PAT")
-)
-
-test_that("warning pops-up when wrong owners are submitted", {
-  expect_warning(git_lab_wrong_groups$get_repos_by_owner())
-})
