3c4880d8563a56c2111ed43697afb2a572e0759a
maciekbanas <banasmaciek@gmail.com>
2023-01-17 09:54:47 +0000

    Use styler.

--- tests/testthat/test-class_github.R
+++ tests/testthat/test-class_github.R
@@ -13,18 +13,18 @@ priv_publ <- environment(git_hub_public$get_repos_by_owner)$private
 priv_enterp <- environment(git_hub_enterprise$get_repos_by_owner)$private
 
 test_that("set_gql_url correctly sets gql api url - for public and private github", {
+  expect_equal(
+    priv_publ$set_gql_url(),
+    "https://api.github.com/graphql"
+  )
 
-  expect_equal(priv_publ$set_gql_url(),
-               "https://api.github.com/graphql")
-
-  expect_equal(priv_enterp$set_gql_url(),
-               "https://github.avengers.com/graphql")
-
+  expect_equal(
+    priv_enterp$set_gql_url(),
+    "https://github.avengers.com/graphql"
+  )
 })
 
 test_that("Check correctly if API url is of Enterprise or Public Github", {
-
   expect_equal(priv_publ$check_enterprise_github(git_hub_public$rest_api_url), FALSE)
   expect_equal(priv_enterp$check_enterprise_github(git_hub_enterprise$rest_api_url), TRUE)
-
 })
