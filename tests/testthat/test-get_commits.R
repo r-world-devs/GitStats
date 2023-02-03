d2a2e38d7aec287f6f9c4db6b22a834c585bdbc9
maciekbanas <banasmaciek@gmail.com>
2023-02-03 08:46:41 +0000

    Add tests for appending commits to database.
    To make second test pass an issue with dates should be resolved first #81.

--- tests/testthat/test-get_commits.R
+++ tests/testthat/test-get_commits.R
@@ -30,7 +30,8 @@ test_that("Get commits prepares table of commits", {
   })
 })
 
-test_that("When storage is set, get commits first saves tables and for the second time it pulls tables from storage and pulls from API only recent commits", {
+test_that("When storage is set, `get_commits()` first saves tables and for the second time it pulls tables from storage
+          and pulls from API only recent commits, and then appends to the database only these new ones", {
 
   test_gitstats <- create_gitstats() %>%
     set_connection(
@@ -46,27 +47,37 @@ test_that("When storage is set, get commits first saves tables and for the secon
   DBI::dbRemoveTable(conn = test_gitstats$storage,
                      name = "commits_by_org")
 
-  commits_stored <- gs_mock("get_commits_by_org_save_to_db",
-    testthat::capture_messages(test_gitstats %>%
+  commits_stored <- testthat::capture_messages(test_gitstats %>%
                                  get_commits(date_from = "2022-10-01",
                                              date_until = "2022-12-31",
                                              print_out = FALSE))
-  )
 
   msgs <- c("`commits_by_org` not found in local database.",
             "All commits will be pulled from API.",
             "`commits_by_org` saved to local database.")
   purrr::walk(msgs, ~expect_match(commits_stored, ., all = FALSE))
 
-  commits_stored <- gs_mock("get_commits_by_org_pull_from_and_append_to_db",
-                            testthat::capture_messages(test_gitstats %>%
-                                                         get_commits(date_from = "2022-10-01",
-                                                                     print_out = FALSE))
-  )
+  gitstats_priv <- environment(test_gitstats$initialize)$private
+
+  commits_before <- gitstats_priv$pull_storage("commits_by_org")
+
+  commits_stored <- testthat::capture_messages(test_gitstats %>%
+                                                 get_commits(date_from = "2022-10-01",
+                                                             print_out = FALSE))
 
   msgs <- c("`commits_by_org` is stored in your local database.",
             "Only commits created since ",
             "`commits_by_org` appended to local database.")
 
   purrr::walk(msgs, ~expect_match(commits_stored, ., all = FALSE))
+
+  commits_after <- gitstats_priv$pull_storage("commits_by_org")
+
+  diff_rows <- nrow(commits_after) - nrow(commits_before)
+
+  expect_gt(diff_rows, 0)
+
+  # expect no duplicates of commits
+  expect_equal(length(unique(commits_after$id)), nrow(commits_after))
+
 })
