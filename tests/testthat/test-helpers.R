34ab31d7150955297c5504663dafc8571a61f08d
maciekbanas <banasmaciek@gmail.com>
2023-02-16 12:12:45 +0000

    Add test for basic helper.

--- tests/testthat/test-helpers.R
+++ tests/testthat/test-helpers.R
@@ -1,3 +1,17 @@
+test_that("`get_response()` returns proper status", {
+
+  bad_endpoint <- "https://api.github.com/orgs/everybody_loves_somebody"
+
+  expect_message(
+    get_response(
+      endpoint = bad_endpoint,
+      token = Sys.getenv("GITHUB_PAT")
+    ),
+    "HTTP 404 No such address"
+  )
+
+})
+
 test_that("`gql_response()` returns list", {
   gql_query <- GraphQLQuery$new()
   query <- gql_query$groups_by_user("maciekbanas")
