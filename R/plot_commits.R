203382185cd3cd0eff63aa743f90c89d77e9cf1b
marcink6 <karolina.marcinkowska@contractors.roche.com>
2023-05-05 14:03:31 +0000

    1st version of plot commits by platform

--- R/plot_commits.R
+++ R/plot_commits.R
@@ -10,50 +10,54 @@
 #' @export
 plot_commits <- function(gitstats_obj,
                          time_interval = "month",
-                         group_by = "platform",
-                         panes = FALSE) {
+                         group_by = "platform") {
 
   time_interval <- match.arg(time_interval,
                              c("day", "month", "week"))
-  group_by <- match.arg(group_by,
-                        c("organization", "platform"))
 
-  commits_dt <- gitstats_obj$show_commits()
+  commits_dt <- data.table::copy(gitstats_obj$show_commits())
   if (is.null(commits_dt)) {
     cli::cli_abort("No commits in `GitStats` object to plot.")
   }
-  time_interval <- match.arg(time_interval)
+  # time_interval <- match.arg(time_interval)
 
-  if (time_interval == "day") {
-    commits_dt[, stats_date := committed_date]
-  } else if (time_interval == "week") {
-    commits_dt[, stats_date := paste(format(committed_date, "%-V"), format(committed_date, "%G"), sep = "-")]
-  } else if (time_interval == "month") {
-    commits_dt[, stats_date := as.Date(paste0(substring(committed_date, 1, 7), "-01"))]
-  }
+  commits_dt[, stats_date := ifelse(time_interval == "day",
+                                    as.Date(committed_date),
+                                    ifelse(time_interval == "month",
+                                           format(committed_date, '%Y-%m'),
+                                           format(committed_date, "%G-%V")
+                                    )),
+             .(id)]
+  commits_dt[, platform := stringi::stri_split_fixed(api_url,
+                                                     "/",
+                                                     omit_empty = T,
+                                                     simplify = T)[2],
+           .(id)]
 
-  commits_n <- commits_dt[, .(commits_n = .N), by = .(stats_date, api_url, organization)]
-  commits_n[, platform := api_url]
+  commits_n <- commits_dt[, .(commits_n = .N), by = .(stats_date, platform, organization)]
   commits_n <- commits_n[order(stats_date)]
 
-  if (!panes) {
-    plotly::plot_ly(commits_n,
-                    x = ~ stats_date,
-                    y = ~ commits_n,
-                    color = ~ eval(parse(text = group_by)),
-                    type = "scatter",
-                    mode = "lines+markers",
-                    hoverinfo = "text",
-                    hovertext = ~paste(eval(group_by), ": ", eval(parse(text = group_by)), "\n",
-                                       "Date: ", stats_date, "\n",
-                                       "Number of commits: ", commits_n)
-    ) %>%
-      plotly::layout(title = paste0('Number of commits by ', eval(group_by)),
-                     legend = list(orientation = 'h'),
-                     xaxis = list(title = ''),
-                     yaxis = list(title = ''))
-  } else {
-
-  }
-
+  commits_n %>%
+    split(commits_n$platform) %>%
+    lapply(function(dt) {
+      plotly::plot_ly(data = dt,
+                      x = ~ stats_date,
+                      y = ~ commits_n,
+                      color = ~ organization,
+                      type = "scatter",
+                      mode = "lines+markers",
+                      hoverinfo = "text",
+                      hovertext = ~paste0("Repository: ", organization, "\n",
+                                          "Date: ", stats_date, "\n",
+                                          "Number of commits: ", commits_n)
+      ) %>%
+        plotly::layout(title = "",
+                       legend = list(orientation = 'h'),
+                       xaxis = list(title = ''),
+                       yaxis = list(title = ''))
+    }) %>%
+    plotly::subplot(nrows = NROW(.),
+                    margin = .05
+                    ) %>%
+    plotly::layout(yaxis = list(title = "no. of commits"))
 }
