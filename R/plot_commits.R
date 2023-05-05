7e4188ef7daf17de76ff23b587a57f312a8a0902
maciekbanas <banasmaciek@gmail.com>
2023-05-05 08:53:48 +0000

    Prepare commits plots for improvements.

--- R/plot_commits.R
+++ R/plot_commits.R
@@ -3,43 +3,57 @@
 #' @title Plot commits data.
 #' @name plot_commits
 #' @param gitstats_obj  A GitStats object.
-#' @param stats_by A character, specifying time interval to show statistics.
+#' @param time_interval A character, specifying time interval to show statistics.
+#' @param group_by
+#' @param panes A boolean to decide wether show stats in separate panes.
 #' @return A plot.
 #' @export
 plot_commits <- function(gitstats_obj,
-                         stats_by = c("day", "week", "month")) {
-  stats_date <- committed_date <- .N <- organization <- NULL
+                         time_interval = "month",
+                         group_by = "platform",
+                         panes = FALSE) {
+
+  time_interval <- match.arg(time_interval,
+                             c("day", "month", "week"))
+  group_by <- match.arg(group_by,
+                        c("organization", "platform"))
 
   commits_dt <- gitstats_obj$show_commits()
   if (is.null(commits_dt)) {
     cli::cli_abort("No commits in `GitStats` object to plot.")
   }
-  stats_by <- match.arg(stats_by)
+  time_interval <- match.arg(time_interval)
 
-  if (stats_by == "day") {
+  if (time_interval == "day") {
     commits_dt[, stats_date := committed_date]
-  } else if (stats_by == "week") {
+  } else if (time_interval == "week") {
     commits_dt[, stats_date := paste(format(committed_date, "%-V"), format(committed_date, "%G"), sep = "-")]
-  } else if (stats_by == "month") {
+  } else if (time_interval == "month") {
     commits_dt[, stats_date := as.Date(paste0(substring(committed_date, 1, 7), "-01"))]
   }
 
-  commits_n <- commits_dt[, .(commits_n = .N), by = .(stats_date, organization)]
+  commits_n <- commits_dt[, .(commits_n = .N), by = .(stats_date, api_url, organization)]
+  commits_n[, platform := api_url]
   commits_n <- commits_n[order(stats_date)]
 
-  plotly::plot_ly(commits_n,
-    x = ~stats_date,
-    y = ~commits_n,
-    color = ~organization,
-    type = "scatter",
-    mode = "lines+markers",
-    hoverinfo = "text",
-    hovertext = ~paste("Organization: ", organization, "\n",
-                       "Date: ", stats_date, "\n",
-                       "Number of commits: ", commits_n)
-  ) %>%
-    plotly::layout(title = 'Number of commits by organization',
-                   legend = list(orientation = 'h'),
-                   xaxis = list(title = ''),
-                   yaxis = list(title = ''))
+  if (!panes) {
+    plotly::plot_ly(commits_n,
+                    x = ~ stats_date,
+                    y = ~ commits_n,
+                    color = ~ eval(parse(text = group_by)),
+                    type = "scatter",
+                    mode = "lines+markers",
+                    hoverinfo = "text",
+                    hovertext = ~paste(eval(group_by), ": ", eval(parse(text = group_by)), "\n",
+                                       "Date: ", stats_date, "\n",
+                                       "Number of commits: ", commits_n)
+    ) %>%
+      plotly::layout(title = paste0('Number of commits by ', eval(group_by)),
+                     legend = list(orientation = 'h'),
+                     xaxis = list(title = ''),
+                     yaxis = list(title = ''))
+  } else {
+
+  }
+
 }
