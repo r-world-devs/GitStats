9a43a955de1a4dddd9bdcc10331f80ace9eb1fd8
Maciej Banas <banasmaciek@gmail.com>
2023-06-19 10:08:25 +0000

    Test get_users for GitLab.

--- tests/testthat/test-04-EngineGraphQL.R
+++ tests/testthat/test-04-EngineGraphQL.R
@@ -11,3 +11,17 @@ test_that("get_users build users table for GitHub", {
     users_result
   )
 })
+
+test_gql_gl <- EngineGraphQLGitLab$new(
+  gql_api_url = "https://gitlab.com/api/graphql",
+  token = Sys.getenv("GITLAB_PAT_PUBLIC")
+)
+
+test_that("get_users build users table for GitLab", {
+  users_result <- test_gql$get_users(
+    users = c("maciekbanas", "Cotau", "marcinkowskak")
+  )
+  expect_users_table(
+    users_result
+  )
+})
