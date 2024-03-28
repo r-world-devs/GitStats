dee4cbc6dae2985a11283d61e82e9768559b3d0f
banasm <maciej.banas@contractors.roche.com>
2024-03-28 10:18:20 +0000

    Try fix function.

--- R/GitHostGitLab.R
+++ R/GitHostGitLab.R
@@ -99,7 +99,21 @@ GitHostGitLab <- R6::R6Class("GitHostGitLab",
     # check token scopes
     # response parameter only for need of super method
     check_token_scopes = function(response = NULL, token) {
-      TRUE
+      token_scopes <- httr2::request(private$endpoints$tokens) %>%
+        httr2::req_headers("Authorization" = paste0("Bearer ", token)) %>%
+        httr2::req_perform() %>%
+        httr2::resp_body_json() %>%
+        purrr::keep(~ .$active) %>%
+        purrr::map(function(pat) {
+          data.frame(scopes = unlist(pat$scopes), date = pat$last_used_at)
+        }) %>%
+        purrr::list_rbind() %>%
+        dplyr::filter(
+          date == max(date)
+        ) %>%
+        dplyr::select(scopes) %>%
+        unlist()
+      any(private$access_scopes %in% token_scopes)
     },
 
     # Add `api_url` column to table.
