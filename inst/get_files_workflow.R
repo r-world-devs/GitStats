f4d79471874b2335e7590258dad2419924e763a1
Maciej Banas <maciej.banas@contractors.roche.com>
2024-09-11 14:02:40 +0000

    Update workflow. This part is superfluous after adjusting `get_files_content()`.

--- inst/get_files_workflow.R
+++ inst/get_files_workflow.R
@@ -16,17 +16,6 @@ md_files_structure <- get_files_structure(
 
 get_files_content(test_gitstats)
 
-purrr::imap(logo_files_structure[[1]]$openpharma, function(repository, repository_name) {
-  create_gitstats() |>
-    set_github_host(
-      repos = paste0("openpharma/", repository_name)
-    ) |>
-    get_files_content(
-      file_path = repository
-    )
-}) %>%
-  purrr::list_rbind()
-
 md_files_structure <- get_files_structure(
   gitstats_obj = test_gitstats,
   pattern = "\\.md|\\.qmd|\\.Rmd",
