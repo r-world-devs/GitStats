c25945c44a6a94b1359ff2babe25fd27dc20d5db
Maciej Banas <maciej.banas@contractors.roche.com>
2024-09-27 11:15:54 +0000

    Bump version, add job to check version bumping, mock some tests with fixtures.

--- tests/testthat/helper-fixtures.R
+++ tests/testthat/helper-fixtures.R
@@ -29,3 +29,40 @@ test_fixtures$half_empty_gql_response <- list(
     )
   )
 )
+
+test_fixtures$github_file_response <- list(
+  "data" = list(
+    "repository" = list(
+      "repo_id"   = "01010101",
+      "repo_name" = "TestProject",
+      "repo_url"  = "https://github.com/r-world-devs/GitStats",
+      "file" = list(
+        "text" = "Some interesting text.",
+        "byteSize" = 50L
+      )
+    )
+  )
+)
+
+test_fixtures$gitlab_search_response <- list(
+  list(
+    "basename"   = "test",
+    "data"       = "some text with searched phrase",
+    "path"       = "test.R",
+    "filename"   = "test.R",
+    "id"         = NULL,
+    "ref"        = "main",
+    "startline"  = 10,
+    "project_id" = 43398933
+  ),
+  list(
+    "basename"   = "test",
+    "data"       = "some text with searched phrase",
+    "path"       = "test.R",
+    "filename"   = "test.R",
+    "id"         = NULL,
+    "ref"        = "main",
+    "startline"  = 15,
+    "project_id" = 43400864
+  )
+)
