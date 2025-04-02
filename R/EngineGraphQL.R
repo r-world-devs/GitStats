8f4c0363b67d01fa410da187d1d2e4496a06b113
Maciej Banas <maciej.banas@contractors.roche.com>
2025-04-02 11:37:17 +0000

    Fix pulling issues, when description field is NULL.

--- R/EngineGraphQL.R
+++ R/EngineGraphQL.R
@@ -68,6 +68,7 @@ EngineGraphQL <- R6::R6Class(
                                     org) {
       issues_table <- purrr::imap(repos_list_with_issues, function(repo, repo_name) {
         issues_row <- purrr::map_dfr(repo, function(issue_data) {
+          issue_data[["node"]][["description"]] <- issue_data[["node"]][["description"]] %||% ""
           issue_data[["node"]][["number"]] <- as.character(issue_data[["node"]][["number"]])
           issue_data[["node"]][["created_at"]] <- lubridate::as_datetime(issue_data[["node"]][["created_at"]])
           issue_data[["node"]][["closed_at"]] <- lubridate::as_datetime(issue_data[["node"]][["closed_at"]] %||% "")
