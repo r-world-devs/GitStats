4de3dedf4abb7ffcad9fa9ca0ebc0831398ca742
Maciej Banas <maciej.banas@contractors.roche.com>
2025-06-25 06:55:02 +0000

    Add browsers for test reasons.

--- R/error_handlers.R
+++ R/error_handlers.R
@@ -3,9 +3,14 @@ handle_graphql_error <- function(responses_list, verbose) {
     if (verbose) cli::cli_alert_danger("GraphQL returned errors.")
     if (inherits(responses_list, "graphql_no_fields_error")) {
       if (verbose) {
-        error_fields <- purrr::map_vec(responses_list$errors, ~.$extensions$fieldName)
-        cli::cli_alert_info("Your GraphQL does not recognize [{error_fields}] field{?s}.")
-        cli::cli_alert_warning("Check version of your GitLab.")
+        tryCatch({
+          error_fields <- purrr::map_vec(responses_list$errors, ~.$extensions$fieldName)
+          cli::cli_alert_info("Your GraphQL does not recognize [{error_fields}] field{?s}.")
+          cli::cli_alert_warning("Check version of your GitLab.")
+        },
+        error = function(e) {
+          browser()
+        })
       }
     }
   } else if (any(purrr::map_lgl(responses_list, ~ inherits(., "graphql_error")))) {
@@ -13,9 +18,14 @@ handle_graphql_error <- function(responses_list, verbose) {
     if (verbose) cli::cli_alert_danger("GraphQL returned errors.")
     check <- any(purrr::map_lgl(responses_list, ~ inherits(., "graphql_no_fields_error")))
     if (check && verbose) {
-      error_fields <- purrr::map_vec(responses_list, ~ purrr::map(.$errors, ~.$extensions$fieldName) |> purrr::discard(is.null)) |> unique()
-      cli::cli_alert_info("Your GraphQL does not recognize [{error_fields}] field{?s}.")
-      cli::cli_alert_warning("Check version of your GitLab.")
+      tryCatch({
+        error_fields <- purrr::map_vec(responses_list, ~ purrr::map(.$errors, ~.$extensions$fieldName) |> purrr::discard(is.null)) |> unique()
+        cli::cli_alert_info("Your GraphQL does not recognize [{error_fields}] field{?s}.")
+        cli::cli_alert_warning("Check version of your GitLab.")
+      },
+      error = function(e) {
+        browser()
+      })
     }
   }
   return(responses_list)
