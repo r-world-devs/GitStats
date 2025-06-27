ff6a6623abb6656d7953e7cfafbde39b4d46fd25
Maciej Banas <maciej.banas@contractors.roche.com>
2025-06-27 09:22:50 +0000

    Remove browsers.

--- R/error_handlers.R
+++ R/error_handlers.R
@@ -3,14 +3,9 @@ handle_graphql_error <- function(responses_list, verbose) {
     if (verbose) cli::cli_alert_danger("GraphQL returned errors.")
     if (inherits(responses_list, "graphql_no_fields_error")) {
       if (verbose) {
-        tryCatch({
-          error_fields <- purrr::map_vec(responses_list$errors, ~.$extensions$fieldName)
-          cli::cli_alert_info("Your GraphQL does not recognize [{error_fields}] field{?s}.")
-          cli::cli_alert_warning("Check version of your GitLab.")
-        },
-        error = function(e) {
-          browser()
-        })
+        error_fields <- purrr::map_vec(responses_list$errors, ~.$extensions$fieldName)
+        cli::cli_alert_info("Your GraphQL does not recognize [{error_fields}] field{?s}.")
+        cli::cli_alert_warning("Check version of your GitLab.")
       }
     }
   } else if (any(purrr::map_lgl(responses_list, ~ inherits(., "graphql_error")))) {
@@ -18,14 +13,9 @@ handle_graphql_error <- function(responses_list, verbose) {
     if (verbose) cli::cli_alert_danger("GraphQL returned errors.")
     check <- any(purrr::map_lgl(responses_list, ~ inherits(., "graphql_no_fields_error")))
     if (check && verbose) {
-      tryCatch({
-        error_fields <- purrr::map_vec(responses_list, ~ purrr::map(.$errors, ~.$extensions$fieldName) |> purrr::discard(is.null)) |> unique()
-        cli::cli_alert_info("Your GraphQL does not recognize [{error_fields}] field{?s}.")
-        cli::cli_alert_warning("Check version of your GitLab.")
-      },
-      error = function(e) {
-        browser()
-      })
+      error_fields <- purrr::map_vec(responses_list, ~ purrr::map(.$errors, ~.$extensions$fieldName) |> purrr::discard(is.null)) |> unique()
+      cli::cli_alert_info("Your GraphQL does not recognize [{error_fields}] field{?s}.")
+      cli::cli_alert_warning("Check version of your GitLab.")
     }
   }
   return(responses_list)
