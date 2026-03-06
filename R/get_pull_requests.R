9366d4b6df554b65837d90a0f6a09a8941567056
Karolina Marcinkowska <karolina.marcinkowska@contractors.roche.com>
2026-03-06 12:42:21 +0000

    fix documentation

--- R/get_pull_requests.R
+++ R/get_pull_requests.R
@@ -45,17 +45,12 @@ get_pull_requests <- function(gitstats,
 
 #' @title Get pull requests statistics
 #' @name get_pull_requests_stats
-#' @description Prepare statistics from the pulled issues data.
-#' @details To make function work, you need first to get issues data with
+#' @description Prepare statistics from the pulled pull requests data.
+#' @details To make function work, you need first to get pull requests data with
 #'   `GitStats`. See examples section.
-#' @param issues A `gitstats_issue` S3 class table object (output of `get_issues()`).
-#' @param time_aggregation A character, specifying time aggregation of
-#'   statistics.
-#' @param group_var Other grouping variable to be passed to `dplyr::group_by()`
-#'   function apart from `stats_date` and `githost`. Could be: `author`,
-#'   `state` or `organization`. Should be passed without
-#'   quotation marks.
-#' @return A table of `issues_stats` class.
+#' @param pull_requests A `gitstats_pr_stats` S3 class table object (output of `get_pull_requests()`).
+#' @inheritParams get_issues
+#' @return A table of `pull_requests_stats` class.
 #' @examples
 #' \dontrun{
 #'  my_gitstats <- create_gitstats() |>
